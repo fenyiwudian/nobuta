94e11cde0afdb47a7a8997b85788427259379d45
fenyiwudian <674702221@qq.com>
2017-07-20 20:59:02 +0800

    image crossoring attribute/canvas toDataURL

--- img_to_canvas/index.js
+++ img_to_canvas/index.js
@@ -1,11 +1,25 @@
 var img = document.getElementById('img')
 
 img.onload = function () {
-    var canvas = document.getElementById('canvas')
-
-    canvas.width = img.width;
-    canvas.height = img.height;
-
+    canvas = document.getElementById('canvas')
+    canvas.width = 100;
+    canvas.height = 100;
     canvas.getContext('2d').drawImage(img, 0, 0);
+    var downloadCustom = document.getElementById('download_custom');
+    downloadCustom.setAttribute('href', canvas.toDataURL())
+}   
+
+var getBase64 = function(elem, width, height){
+    html2canvas(elem || document.body, {
+        useCORS:true,
+        width: width || 100,
+        height: height || 100,
+    }).then(function(canvas){
+        var base64 = canvas.toDataURL();
+        console.log(base64)
+        var image = document.createElement('img');
+        image.setAttribute('src', base64)
+        document.body.appendChild(image);
+    })
 }
 
---
+++ prop_count/index.js
@@ -0,0 +1,51 @@
+var propCount = 0;
+var itemCount = 0;
+
+var propObj = {};
+var itemObj = [];
+
+var addProp = function (count) {
+    var startAt = new Date().getTime();
+    var start = propCount;
+    count = count || 1000;
+    var end = propCount + count;
+    for (; start < end; start++) {
+        propObj[start] = true;
+    }
+    propCount = end;
+    var endAt = new Date().getTime();
+    console.log('add count: ', count);
+    console.log('currnet count: ', end);
+    console.log('cost time : ', endAt - startAt);
+}
+
+
+var getPropItem = function (key) {
+    var startAt = new Date().getTime();
+    console.log('item content: ', propObj[key]);
+    var endAt = new Date().getTime();
+    console.log('cost time : ', endAt - startAt);
+}
+
+var addItem = function (count) {
+    var startAt = new Date().getTime();
+    var start = itemCount;
+    count = count || 1000;
+    var end = itemCount + count;
+    for (; start < end; start++) {
+        itemObj.push(start);
+    }
+    itemCount = end;
+    var endAt = new Date().getTime();
+    console.log('add count: ', count);
+    console.log('currnet count: ', end);
+    console.log('cost time : ', endAt - startAt);
+}
+
+
+var getListItem = function (key) {
+    var startAt = new Date().getTime();
+    console.log('item content: ', itemObj[key]);
+    var endAt = new Date().getTime();
+    console.log('cost time : ', endAt - startAt);
+}
