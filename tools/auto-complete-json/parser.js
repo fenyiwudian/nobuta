d135666cf96ac2ac27fbc9c28e7da70c79bd9dbb
fenyiwudian <674702221@qq.com>
2017-10-23 10:55:43 +0800

    auto-complete json

---
+++ tools/auto-complete-json/parser.js
@@ -0,0 +1,150 @@
+let str = `讴歌ACURA	讴歌	ACURA	ouge
+阿尔法·罗密欧ALFA ROMEO	阿尔法·罗密欧	ALFA ROMEO	aerfa·luomiou
+阿尔宾纳ALPINA	阿尔宾纳	ALPINA	aerbinna
+阿斯顿·马丁ASTON MARTIN	阿斯顿·马丁	ASTON MARTIN	asidun·mading
+奥迪AUDI	奥迪	AUDI	aodi
+北汽BAIC	北汽	BAIC	beiqi
+宝骏BAOJUN	宝骏	BAOJUN	baojun
+保隆BAOLONG	保隆	BAOLONG	baolong
+北京BEIJING	北京	BEIJING	beijing
+宾利BENTLEY	宾利	BENTLEY	binli
+奔驰BENZ	奔驰	BENZ	benchi
+比速BISU	比速	BISU	bisu
+宝马BMW	宝马	BMW	baoma
+宝沃BORGWARD	宝沃	BORGWARD	baowo
+巴博斯BRABUS	巴博斯	BRABUS	babosi
+保斐利BUFORI	保斐利	BUFORI	baofeili
+别克BUICK	别克	BUICK	bieke
+比亚迪BYD	比亚迪	BYD	biyadi
+凯迪拉克CADILLAC	凯迪拉克	CADILLAC	kaidilake
+长安CHANGAN	长安	CHANGAN	changan
+长丰CHANGFENG	长丰	CHANGFENG	changfeng
+昌河CHANGHE	昌河	CHANGHE	changhe
+奇瑞CHERY	奇瑞	CHERY	qirui
+雪佛兰CHEVROLET	雪佛兰	CHEVROLET	xuefolan
+克莱斯勒CHRYSLER	克莱斯勒	CHRYSLER	kelaisile
+思铭CIIMO	思铭	CIIMO	siming
+雪铁龙CITROEN	雪铁龙	CITROEN	xuetielong
+旗云COWIN	旗云	COWIN	qiyun
+大迪DA DI	大迪	DA DI	dadi
+达西亚DACIA	达西亚	DACIA	daxiya
+大地DADI	大地	DADI	dadi
+腾势DENZA	腾势	DENZA	tengshi
+东风风神DF FENGSHEN	东风风神	DF FENGSHEN	dongfengfengshen
+东风小康DFSK	东风小康	DFSK	dongfengxiaokang
+道奇DODGE	道奇	DODGE	daoqi
+东风DONGFENG	东风	DONGFENG	dongfeng
+DSDS	DS	DS	DS
+潍柴英致ENRANGER	潍柴英致	ENRANGER	weichaiyingzhi
+ETET	ET	ET	ET
+理念EVERUS	理念	EVERUS	linian
+一汽FAW	一汽	FAW	yiqi
+一汽解放FAW JIAXING	一汽解放	FAW JIAXING	yiqijiefang
+风度FENGDU	风度	FENGDU	fengdu
+法拉利FERRARI	法拉利	FERRARI	falali
+菲亚特FIAT	菲亚特	FIAT	feiyate
+菲斯克FISKER	菲斯克	FISKER	feisike
+福特FORD	福特	FORD	fute
+福田FOTON	福田	FOTON	futian
+福迪FUDI	福迪	FUDI	fudi
+富奇FUQI	富奇	FUQI	fuqi
+广汽GAC	广汽	GAC	guangqi
+吉利GEELY	吉利	GEELY	jili
+GMCGMC	GMC	GMC	GMC
+长城GREAT WALL	长城	GREAT WALL	changcheng
+GUMPERTGUMPERT	GUMPERT	GUMPERT	GUMPERT
+哈飞HAFEI	哈飞	HAFEI	hafei
+海马HAIMA	海马	HAIMA	haima
+航天HANGTIAN	航天	HANGTIAN	hangtian
+汉腾HANTENG	汉腾	HANTENG	hanteng
+哈弗HAVAL	哈弗	HAVAL	hafu
+海格HIGER	海格	HIGER	haige
+本田HONDA	本田	HONDA	bentian
+华颂HUASONG	华颂	HUASONG	huasong
+华泰HUATAI	华泰	HUATAI	huatai
+悍马HUMMER	悍马	HUMMER	hanma
+现代HYUNDAI	现代	HYUNDAI	xiandai
+英菲尼迪INFINITI	英菲尼迪	INFINITI	yingfeinidi
+五十铃ISUZU	五十铃	ISUZU	wushiling
+江淮JAC	江淮	JAC	jianghuai
+捷豹JAGUAR	捷豹	JAGUAR	jiebao
+JEEPJEEP	JEEP	JEEP	JEEP
+江铃JIANGLING	江铃	JIANGLING	jiangling
+江南JIANGNAN	江南	JIANGNAN	jiangnan
+金杯JINBEI	金杯	JINBEI	jinbei
+金程JINCHENG	金程	JINCHENG	jincheng
+金旅JINLV	金旅	JINLV	jinlv
+永源JONWAY	永源	JONWAY	yongyuan
+开瑞KARRY	开瑞	KARRY	kairui
+卡威KAWEI	卡威	KAWEI	kawei
+起亚KIA	起亚	KIA	qiya
+兰博基尼LAMBORGHINI	兰博基尼	LAMBORGHINI	lanbojini
+路虎LANDROVER	路虎	LANDROVER	luhu
+陆风LANDWIND	陆风	LANDWIND	lufeng
+领志LEAHEAD	领志	LEAHEAD	lingzhi
+雷克萨斯LEXUS	雷克萨斯	LEXUS	leikesasi
+力帆LIFAN	力帆	LIFAN	lifan
+林肯LINCOLN	林肯	LINCOLN	linken
+路特斯LOTUS	路特斯	LOTUS	lutesi
+纳智捷LUXGEN	纳智捷	LUXGEN	nazhijie
+玛莎拉蒂MASERATI	玛莎拉蒂	MASERATI	mashaladi
+迈巴赫MAYBACH	迈巴赫	MAYBACH	maibahe
+马自达MAZDA	马自达	MAZDA	mazida
+迈凯伦MCLAREN	迈凯伦	MCLAREN	maikailun
+名爵MG	名爵	MG	mingjue
+MINIMINI	MINI	MINI	MINI
+三菱MITSUBISHI	三菱	MITSUBISHI	sanling
+日产NISSAN	日产	NISSAN	richan
+奥路卡OLUCK	奥路卡	OLUCK	aoluka
+欧宝OPEL	欧宝	OPEL	oubao
+标致PEUGEOT	标致	PEUGEOT	biaozhi
+庞蒂克PONTIAC	庞蒂克	PONTIAC	pangdike
+保时捷PORSCHE	保时捷	PORSCHE	baoshijie
+宝腾PROTON	宝腾	PROTON	baoteng
+启腾QITENG	启腾	QITENG	qiteng
+观致QOROS	观致	QOROS	guanzhi
+朗世RANZ	朗世	RANZ	langshi
+威麟RELY	威麟	RELY	weilin
+雷诺RENAULT	雷诺	RENAULT	leinuo
+瑞麒RIICH	瑞麒	RIICH	ruiqi
+荣威ROEWE	荣威	ROEWE	rongwei
+劳斯莱斯ROLLS ROYCE	劳斯莱斯	ROLLS ROYCE	laosilaisi
+西雅图SEAT	西雅图	SEAT	xiyatu
+守望SHOUWANG	守望	SHOUWANG	shouwang
+双环SHUANGHUAN	双环	SHUANGHUAN	shuanghuan
+斯柯达SKODA	斯柯达	SKODA	sikeda
+SMARTSMART	SMART	SMART	SMART
+东南SOUEAST	东南	SOUEAST	dongnan
+双龙SSANGYONG	双龙	SSANGYONG	shuanglong
+斯巴鲁SUBARU	斯巴鲁	SUBARU	sibalu
+铃木SUZUKI	铃木	SUZUKI	lingmu
+斯威SWM	斯威	SWM	siwei
+泰卡特TECHART	泰卡特	TECHART	taikate
+特斯拉TESLA	特斯拉	TESLA	tesila
+天马TIANMA	天马	TIANMA	tianma
+天津一汽TJ FAW	天津一汽	TJ FAW	tianjinyiqi
+丰田TOYOTA	丰田	TOYOTA	fengtian
+启辰VENUCIA	启辰	VENUCIA	qichen
+沃尔沃VOLVO	沃尔沃	VOLVO	woerwo
+大众VW	大众	VW	dazhong
+魏派WEY	魏派	WEY	weipai
+五菱WULING	五菱	WULING	wuling
+中华ZHONGHUA	中华	ZHONGHUA	zhonghua
+中兴ZHONGXING	中兴	ZHONGXING	zhongxing
+之诺ZINORO	之诺	ZINORO	zhinuo
+众泰ZOTYE	众泰	ZOTYE	zhongtai`;
+
+
+const lines = str.split('\n');
+
+const list = lines.map(line => {
+    const props = line.split('\t');
+    const [name, ...triggers] = props;
+    return {
+        name,
+        triggers
+    }
+    
+})
+
+console.log(JSON.stringify(list));
