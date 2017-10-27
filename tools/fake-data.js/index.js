c1c319598ee07c9edfa0949a85d8559d565911c8
fenyiwudian <674702221@qq.com>
2017-10-27 16:26:38 +0800

    fake-data format

---
+++ tools/fake-data.js/index.js
@@ -0,0 +1,18 @@
+a = {
+    "NodeID": "5B408731-C0B1-4036-AEF1-7D1EED8CFB6D",
+    "NTitle": "SELECT_3",
+    "NType": "N",
+    "OptionRID": "AC6F339D-BB5E-4135-9559-D1F19F0EBA19",
+    "OptionCID": "00000000-0000-0000-0000-000000000000",
+    "OptionRContent": "珀莱雅",
+    "OptionCContent": "",
+    "OptionRNumber": 3,
+    "OptionCNumber": 0,
+    "Context": "",
+    "Source1OptionID": "00000000-0000-0000-0000-000000000000",
+    "Source2OptionID": "00000000-0000-0000-0000-000000000000",
+    "RankResult": 0,
+    "IsOther": false,
+    "CostTime": 5181,
+    "duration": 0
+};
