65cbc0959be0b444befafcb9dcc2531bd360407d
fenyiwudian <674702221@qq.com>
2017-11-10 20:00:03 +0800

    promise-err

---
+++ tricks/promise-err.js
@@ -0,0 +1,78 @@
+var http = require('http');
+
+
+const requestSync_2 = () => {
+    return new Promise(resolve => {
+        const req = http.request({
+            host: 'localhost',
+            port: 10003,
+            path: '/plugin/da_tong_01.html',
+            method: 'GET',
+        }, (res) => {
+            res.on('data', () => {
+            });
+            res.on('end', () => {
+                resolve(222);
+            });
+        });
+
+        req.end();
+    });
+};
+
+const reqeustSync_1 = () => {
+    return new Promise(resolve => {
+        const req = http.request({
+            host: 'localhost',
+            port: 10003,
+            path: '/plugin/car.js',
+            method: 'GET',
+        }, (res) => {
+            res.on('data', () => {});
+            res.on('end', () => {
+                requestSync_2().then(res => {
+                    resolve(res);
+                });
+            });
+        });
+        req.on('error', (err) => {
+            console.log(err);
+        });
+        req.end();
+    });
+};
+
+const timeoutSync_2 = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('time out 2');
+        }, 500);
+    });
+};
+
+
+const timeoutSync_1 = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            timeoutSync_2().then(res => {
+                resolve(res);
+            });
+        }, 500);
+    });
+};
+
+const main = () => {
+    reqeustSync_1().then(res => {
+        console.log('request success', res);
+    }).then(() => {
+        return timeoutSync_1().then(res => {
+            console.log('timeout success', res);
+            return res;
+        });
+    }).catch(err => {
+        console.log('outer catch err:', err);
+    });
+};
+
+
+main();
