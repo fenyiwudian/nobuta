7ab01d978f213103bd01e2e0c99ed1ca6ffaf375
fenyiwudian <674702221@qq.com>
2018-03-27 20:48:57 +0800

    string replacing compress test 1

---
+++ client_demo/text-replace/index.js
@@ -0,0 +1,76 @@
+var obj = [{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":985,"y":293,"nodeName":"END_9","type":10,"categoryType":2,"commonType":"TERMINAL","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[],"used":true,"nodeUuid":"86BD8A48-A0EC-4C67-A59D-A2D1EC3B945B","duration":0,"hasVar":false,"favorite":false,"images":[],"questionText":"谢谢您的参与","describe":"","input":{"index":-2,"uuid":"46849B5E-0A2D-49A9-9FE5-B3DDDBBC600C"},"request":{"url":"","method":1,"params":[],"headers":[]},"linkUrl":"","extField_1":"","extField_2":"","endBtnText":"","useReward":false},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":530,"y":307,"nodeName":"SELECT_PIC_1","type":23,"categoryType":3,"commonType":"SUBJECT","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[{"selfPortId":"5E68D708-C861-4BAD-9E7F-24538470667A","destInputId":"93BB75E7-783B-4B14-90D1-9F2572301A4A"}],"used":true,"nodeUuid":"E0FD9A74-2C43-4819-9CD4-31F985366401","duration":0,"hasVar":true,"favorite":false,"images":[],"questionText":"111","describe":"","options":[{"nodeType":23,"number":1,"typeFlag":"R","index":0,"text":"11","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"1F8E3915-7F0E-494A-BE2E-A6BA6A94B958","groupNumber":0,"imageId":"A55FBEEC","imageName":"","imageOriginName":"111","imageWidth":0,"imageHeight":0,"image":{"id":"A55FBEEC","name":"","originName":"111","width":0,"height":0}},{"nodeType":23,"number":2,"typeFlag":"R","index":1,"text":"22","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"723B1D53-B4EC-4C7B-8561-379BDB929640","groupNumber":0,"imageId":"6733ACEE-DD78-4EAF-847C-6F9452A193F3","imageName":"C09F01EF025A1E6C39A69FFCF9F97FCA.jpg","imageOriginName":"马.jpg","imageWidth":293,"imageHeight":220,"image":{"id":"6733ACEE-DD78-4EAF-847C-6F9452A193F3","name":"C09F01EF025A1E6C39A69FFCF9F97FCA.jpg","originName":"马.jpg","width":293,"height":220}}],"refOptions":[],"optionRefers":[],"input":{"index":-2,"uuid":"77ACA6D0-F422-4109-9077-71D2F607BDCF"},"output":{"index":-1,"uuid":"5E68D708-C861-4BAD-9E7F-24538470667A"},"validation":null,"randomOptionY":"N","answerRequired":"Y","hasMutexOpt":false,"extField_1":"N","extField_2":0,"extField_3":2,"extField_9":"N","extField_4":2,"extField_7":1,"needCornerMark":"N"},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":764,"y":317,"nodeName":"SLIDE_RATE_7","type":42,"categoryType":3,"commonType":"SUBJECT","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[{"selfPortId":"61290D70-6133-4743-B485-1B438FF85058","destInputId":"46849B5E-0A2D-49A9-9FE5-B3DDDBBC600C"}],"used":true,"nodeUuid":"66D9D75A-4064-4467-AF34-6E8A2C61ABDA","duration":0,"hasVar":true,"favorite":false,"images":[],"questionText":"222","describe":"","options":[{"nodeType":42,"number":1,"typeFlag":"R","index":0,"text":"&nbsp;<span class=\"var-tag var-string\" data-id=\"CAB64C38:A6675AAE:OIN\">FILL_2:1 111111:选项输入内容</span>&nbsp;","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"AC241035-217B-407A-A8DC-9CD3B1B58DA1","groupNumber":0,"imageId":"6733ACEE-DD78-4EAF-847C-6F9452A193F3","imageName":"C09F01EF025A1E6C39A69FFCF9F97FCA.jpg","imageOriginName":"马.jpg","imageWidth":293,"imageHeight":220,"image":{"id":"6733ACEE-DD78-4EAF-847C-6F9452A193F3","name":"C09F01EF025A1E6C39A69FFCF9F97FCA.jpg","originName":"马.jpg","width":293,"height":220}},{"nodeType":42,"number":2,"typeFlag":"R","index":1,"text":"111111","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"55A044A8-31E2-4CEA-8B47-45E9318720D7","groupNumber":0,"imageId":"E851E46A-C73E-4197-80A6-31F7A1372F3B","imageName":"DE4978464E08B459BF01AE7C4A565E2C.jpg","imageOriginName":"娃娃.jpg","imageWidth":292,"imageHeight":220,"image":{"id":"E851E46A-C73E-4197-80A6-31F7A1372F3B","name":"DE4978464E08B459BF01AE7C4A565E2C.jpg","originName":"娃娃.jpg","width":292,"height":220}}],"refOptions":[],"optionRefers":[],"input":{"index":-2,"uuid":"93BB75E7-783B-4B14-90D1-9F2572301A4A"},"output":{"index":-1,"uuid":"61290D70-6133-4743-B485-1B438FF85058"},"validation":null,"randomOptionY":"N","answerRequired":"Y","hasMutexOpt":false,"extField_9":"N","showType":1,"rateOptions":[{"text":"3","uuid":"2A78AD86-1E5A-4A19-9719-AD92D3AFAC8B","iconId":200,"imageName":"0948af33-422c-4f15-b3f4-31454f583dfb.svg","icon":{"id":200,"originName":"对1","name":"0948af33-422c-4f15-b3f4-31454f583dfb.svg"},"value":1},{"text":"4","uuid":"15A6D973-F90F-46A6-ABB7-65CC9E156C46","iconId":226,"imageName":"bf7d8a9c-aa51-46c6-807b-2b0d8e480317.svg","icon":{"id":226,"originName":"差2","name":"bf7d8a9c-aa51-46c6-807b-2b0d8e480317.svg"},"value":2}]},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":1,"x":-541,"y":270,"nodeName":"Start","type":6,"categoryType":2,"commonType":"TERMINAL","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":1,"destList":[{"selfPortId":"412452F5-0FDE-4B15-9E64-3CA99BCE2D63","destInputId":"3A4BEB7C-01E1-4278-9537-981B936945AE"}],"used":true,"nodeUuid":"2AB80135-80CC-4BAC-B288-B8AD972C305A","duration":0,"hasVar":false,"favorite":false,"images":[],"questionText":"&nbsp;","describe":"","output":{"index":-1,"uuid":"412452F5-0FDE-4B15-9E64-3CA99BCE2D63"},"Language":1,"displayType":1,"goBackEnabled":"Y","qsnrName":"输入和打分验证","startName":"","startBtnText":"","prevBtnText":"","nextBtnText":"","useCustomLogo":false,"containerSizeId":1,"containerSize":"auto","customContainerSize":484,"fontSizeId":1,"fontSize":"LANG.global.small","useLimitTime":true,"limitTime":1800,"timeoutText":"","useMinTime":true,"minTime":60,"timeLessText":"","qNumberRequired":false,"asterisksRequired":false,"progressBarRequired":false,"nodeTypeInfoRequired":false,"noWxShare":true,"bgImageMode":false,"previewTimeLog":false,"autoCpltGroups":[],"fakedServers":["https://editor.cform.io/plugin/common"]},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":-296,"y":243,"nodeName":"GRAPH_MARK_2","type":2,"categoryType":3,"commonType":"SUBJECT","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[{"selfPortId":"430CF434-E266-44F7-9982-2267EC63B0EE","destInputId":"A6A8C9C2-9455-45CF-B6A7-6FF9325CB71E"}],"used":true,"nodeUuid":"F1530538-CE79-4D05-B1D8-BB51AFAE9C6A","duration":0,"hasVar":false,"favorite":false,"images":[],"questionText":"1","describe":"","options":[{"nodeType":2,"number":1,"typeFlag":"R","index":0,"text":"2","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"26B79318-FF66-4BC6-AEF4-4B0F158DE803","groupNumber":0}],"refOptions":[],"optionRefers":[],"input":{"index":-2,"uuid":"3A4BEB7C-01E1-4278-9537-981B936945AE"},"output":{"index":-1,"uuid":"430CF434-E266-44F7-9982-2267EC63B0EE"},"validation":null,"randomOptionY":"N","answerRequired":"Y","hasMutexOpt":false,"extField_9":"N","extField_1":2,"extField_2":1,"extField_3":5,"markImgName":"rating-love","extField_7":"N"},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":-57,"y":243,"nodeName":"FILL_2","type":4,"categoryType":3,"commonType":"SUBJECT","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[{"selfPortId":"89364014-6AAD-4046-8291-3BC6669E947B","destInputId":"DE2D46C0-ACB3-4E7C-B7A5-3CAFCC693690"}],"used":true,"nodeUuid":"6816CA03-4154-4FA4-AEA8-D79BCAB64C38","duration":0,"hasVar":false,"favorite":false,"images":[],"questionText":"1","describe":"","options":[{"nodeType":4,"number":1,"typeFlag":"R","index":0,"text":"111111","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"6F462D37-65A3-4786-AA1F-FA3BA6675AAE","groupNumber":0,"extField_5":7,"extField_4":"N"}],"refOptions":[],"optionRefers":[],"input":{"index":-2,"uuid":"A6A8C9C2-9455-45CF-B6A7-6FF9325CB71E"},"output":{"index":-1,"uuid":"89364014-6AAD-4046-8291-3BC6669E947B"},"validation":null,"randomOptionY":"N","answerRequired":"N","hasMutexOpt":false},{"questionairId":"5aad7625-40e0-4129-b125-9c4942f6bc63","mode":0,"sortNo":-1,"x":176,"y":243,"nodeName":"SELECT_23","type":1,"categoryType":3,"commonType":"SUBJECT","circleNode1Id":"","circleNode2Id":"","parentRandomId":"","order":-1,"destList":[{"selfPortId":"133E5F15-E328-41D3-B3E3-38F9E4AF1292","destInputId":"77ACA6D0-F422-4109-9077-71D2F607BDCF"}],"used":true,"nodeUuid":"EC8A1E36-F590-4CD9-A9BC-5832ECCA39D0","duration":0,"hasVar":true,"favorite":false,"images":[],"questionText":"1","describe":"","options":[{"nodeType":1,"number":1,"typeFlag":"R","index":0,"text":"&nbsp;<span class=\"var-tag var-string\" data-id=\"CAB64C38:A6675AAE:OIN\">FILL_2:1 :选项输入内容</span>&nbsp;","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"3796A720-3A68-41FA-83A7-D3251291DCF3","groupNumber":0},{"nodeType":1,"number":2,"typeFlag":"R","index":1,"text":"3","mutexNumber":0,"useComment":"N","extField_9":1,"uuid":"BB38EF40-8E93-4286-A7AB-ED7C1CE5C49A","groupNumber":0}],"refOptions":[],"optionRefers":[],"input":{"index":-2,"uuid":"DE2D46C0-ACB3-4E7C-B7A5-3CAFCC693690"},"output":{"index":-1,"uuid":"133E5F15-E328-41D3-B3E3-38F9E4AF1292"},"validation":null,"randomOptionY":"N","answerRequired":"Y","hasMutexOpt":false,"extField_9":"N","extField_1":"N","extField_2":0,"extField_3":2}];
+var originText = JSON.stringify(obj);
+var replaceText = originText;
+/** 
+ * 需要使用小字符串替代大字符串,并留下替换列表的方法来减少字符的大小,达到类似压缩的效果.
+ * 先要把uuid替换出来,保证uuid不会被分开替换掉,因为uuid很关键,后台复制的时候会去寻找,不能被分割替换.
+ */
+var heads = '@#%&!*()';
+var headLen = heads.length;
+var headIndex = 0;
+var tails = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var tailLen = tails.length;
+var tailIndex = 0;
+
+var getNextKey = function(){
+    var key = '';
+    do {
+        var rs = heads[headIndex] + tails[tailIndex];
+        if(replaceText.indexOf(rs) < 0){
+            key = rs;
+        }else{
+            tailIndex += 1;
+            if(tailIndex >= tailLen){
+                tailIndex = 0;
+                headIndex += 1;
+                if(headIndex >= headLen){
+                    throw 'keys not enough';
+                }
+            }
+        }
+    }while(!key);
+    return key;
+};
+
+var maps = [];
+
+var replaceId = function(reg){
+    var handled = [];
+    var matches = replaceText.match(reg);
+    if(matches){
+        matches.forEach(function(id){
+            if(handled.indexOf(id) < 0){
+                handled.push(id);
+                var key = getNextKey();
+                maps.push([key, id]);
+                replaceText = replaceText.replace(new RegExp(id, 'g'), key);
+            }
+        });
+    }
+};
+
+var replaceUuid = function(){
+    // 长uuid
+    replaceId(/\w{8}(-\w{4}){3}-\w{12}/g);
+    // 短id(仅有大写的)
+    replaceId(/[0-9A-Z]{8}/g);
+};
+
+
+
+var replace = function(){
+    replaceUuid();
+    console.log(originText.length);
+    console.log(replaceText.length);
+    console.log(maps);
+};
+
+var recover = function(){
+    while(maps.length){
+        var map = maps.pop();
+        replaceText = replaceText.replace(new RegExp(map[0], 'g'), map[1]);
+    }
+    console.log(replaceText.length);
+};
+
+
